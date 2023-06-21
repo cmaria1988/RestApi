5d5cd56c8b7342dbedf27945530d4f1e02c00102
Maria <claumaria1988@gmail.com>
2023-06-21 22:43:52 +0800

    changes

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -25,6 +25,14 @@ class TaskPolicy
     }
 
     /**
+     * Determine whether the user can create the model.
+     */
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    /**
      * Determine whether the user can update the model.
      */
     public function update(User $user, Task $task): bool
